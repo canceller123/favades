f7d4e559142de5048069fd6b9b0153b90a00203f
canceller123 <126961022+canceller123@users.noreply.github.com>
2023-04-18 22:44:14 +0300

    Updating
    used map to store players

--- server.js
+++ server.js
@@ -1,30 +1,66 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = socketIO(server);
 
-const port = 3000;
+const port = 40200;
 
-// Serve static files
-app.use(express.static(__dirname + '/public'));
+// mapp instead of an object for storing players
+const players = new Map();
 
-// Handle socket.io connections
+app.use(express.static(__dirname + '/client'));
+
 io.on('connection', socket => {
   console.log('A player has joined');
 
-  // Listen for movement events
+  let lastPosition = null;
+  let currentPosition = { x: 250, y: 250 };
+
   socket.on('movement', data => {
-    // Broadcast movement to all other players
-    socket.broadcast.emit('movement', data);
+    lastPosition = currentPosition;
+    currentPosition = data;
+
+    //snd movement only to the players in the same room
+    socket.to('game').emit('movement', { id: socket.id, position: currentPosition });
   });
 
-  // Listen for disconnect events
   socket.on('disconnect', () => {
     console.log('A player has disconnected');
+    players.delete(socket.id);
+    socket.to('game').emit('playerDisconnected', socket.id);
   });
+
+  // join the player to the game room
+  socket.join('game');
+
+  //add the player to the players Map
+  players.set(socket.id, currentPosition);
+
+  // Emit a 'playerConnected' event to all other players in the same room
+  socket.to('game').emit('playerConnected', { id: socket.id, position: currentPosition });
+
+  //send the list of players to the new player
+  socket.emit('init', { players: Array.from(players.entries()).map(([id, position]) => ({ id, position })) });
+
+  // Interpolate player positions at a more accurate interval
+  let lastTimestamp = Date.now();
+  setInterval(() => {
+    const currentTimestamp = Date.now();
+    const timeDelta = currentTimestamp - lastTimestamp;
+    lastTimestamp = currentTimestamp;
+
+    if (lastPosition !== null) {
+      const delta = { x: currentPosition.x - lastPosition.x, y: currentPosition.y - lastPosition.y };
+      currentPosition = { x: currentPosition.x + delta.x * (timeDelta / 1000), y: currentPosition.y + delta.y * (timeDelta / 1000) };
+
+      socket.to('game').emit('movement', { id: socket.id, position: currentPosition });
+    }
+  }, 1000 / 60);
 });
 
-// Start the server
-http.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
